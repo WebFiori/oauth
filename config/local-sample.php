6261224b83fd149c7f2bf66338fee9cb4f957921
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-09-03 00:56:39 +0300

    fix: Names of Env Vars

--- config/local-sample.php
+++ config/local-sample.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'microsoft' => [
-        'client_id' => getenv('MICROSOFT_CLIENT_ID') ?: 'your-client-id-here',
-        'client_secret' => getenv('MICROSOFT_CLIENT_SECRET') ?: 'your-client-secret-here',
-        'redirect_uri' => getenv('MICROSOFT_REDIRECT_URI') ?: 'http://localhost:8080/callback',
-        'tenant_id' => getenv('MICROSOFT_TENANT_ID') ?: 'common',
+        'client_id' => getenv('MS_CLIENT_ID') ?: 'your-client-id-here',
+        'client_secret' => getenv('MS_CLIENT_SECRET') ?: 'your-client-secret-here',
+        'redirect_uri' => getenv('MS_REDIRECT_URI') ?: 'http://localhost:8080/callback',
+        'tenant_id' => getenv('MS_TENANT_ID') ?: 'common',
         'refresh_token' => getenv('MS_RFR_TOKEN') ?: 'your-refresh-token-here',
     ]
 ];
