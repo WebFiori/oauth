0ffe948dc5b4d10a6e113eaac310e72c191ff3aa
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-09-03 00:31:37 +0300

    Update local-sample.php

--- config/local-sample.php
+++ config/local-sample.php
@@ -4,6 +4,7 @@ return [
         'client_id' => getenv('MICROSOFT_CLIENT_ID') ?: 'your-client-id-here',
         'client_secret' => getenv('MICROSOFT_CLIENT_SECRET') ?: 'your-client-secret-here',
         'redirect_uri' => getenv('MICROSOFT_REDIRECT_URI') ?: 'http://localhost:8080/callback',
-        'tenant_id' => getenv('MICROSOFT_TENANT_ID') ?: 'common'
+        'tenant_id' => getenv('MICROSOFT_TENANT_ID') ?: 'common',
+        'refresh_token' => getenv('MS_RFR_TOKEN') ?: 'your-refresh-token-here',
     ]
 ];
