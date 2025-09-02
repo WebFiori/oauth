292a72c2c4ae723ab5bdebbd1ad30df86e9730b8
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-09-03 01:08:25 +0300

    Update local-sample.php

--- config/local-sample.php
+++ config/local-sample.php
@@ -3,7 +3,7 @@ return [
     'microsoft' => [
         'client_id' => getenv('MS_CLIENT_ID') ?: 'your-client-id-here',
         'client_secret' => getenv('MS_CLIENT_SECRET') ?: 'your-client-secret-here',
-        'redirect_uri' => getenv('MS_REDIRECT_URI') ?: 'http://localhost:8080/callback',
+        'redirect_uri' => getenv('MS_REDIRECT_URI') ?: 'http://localhost:8080/auth.php',
         'tenant_id' => getenv('MS_TENANT_ID') ?: 'common',
         'refresh_token' => getenv('MS_RFR_TOKEN') ?: 'your-refresh-token-here',
     ]
